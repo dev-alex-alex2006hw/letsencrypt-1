de754573861284c4e31c6c04b7cbf956451d5ac0
Adam Stankiewicz <sheerun@sher.pl>
2016-01-03 22:38:00 +0100

    Also populate /etc/letsencrypt/haproxy directory

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -8,5 +8,8 @@ while read line; do
   KEY_FILE="$line"
   CERT_FILE="$(dirname $line)/fullchain.pem"
   HAPROXY_FILE="$(dirname $line)/haproxy.pem"
+  DOMAIN_NAME="$(basename "$(dirname $line)")"
   cat "$CERT_FILE" "$KEY_FILE" > $HAPROXY_FILE
+  mkdir -p /etc/letsencrypt/haproxy
+  cp "$HAPROXY_FILE" "/etc/letsencrypt/haproxy/$DOMAIN_NAME.pem"
 done
